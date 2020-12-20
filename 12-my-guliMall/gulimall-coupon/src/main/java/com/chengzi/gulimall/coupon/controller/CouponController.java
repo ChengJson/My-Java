79fc52d4ac66aad5a513a5a4c80a5692ab48c32a
chengli <570197298@qq.com>
2020-12-20 22:10:52 +0800

    谷粒商城网关

--- 12-my-guliMall/gulimall-coupon/src/main/java/com/chengzi/gulimall/coupon/controller/CouponController.java
+++ 12-my-guliMall/gulimall-coupon/src/main/java/com/chengzi/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +26,24 @@ import com.chengzi.common.utils.R;
  * @email 570197298@qq.com@gmail.com
  * @date 2020-12-20 17:20:22
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+
+        return R.ok().put("name",name).put("age",age);
+    }
     /**
      * 列表
      */
@@ -40,7 +54,12 @@ public class CouponController {
 
         return R.ok().put("page", page);
     }
-
+    @RequestMapping("/member/list")
+    public R membercoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满100减10");
+        return R.ok().put("coupons",Arrays.asList(couponEntity));
+    }
 
     /**
      * 信息
---
+++ 12-my-guliMall/gulimall-gateway/src/main/java/com/chengzi/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.chengzi.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient//服务发现注册
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
